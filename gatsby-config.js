c90e20a52f8ed8e441adf08b0ee6a3970156fd03
Bilal Tariq <bilaltariq690@hotmail.com>
2022-04-16 19:19:18 +0100

    initial website

---
+++ gatsby-config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: "@wkocjan/gatsby-theme-intro",
+      options: {
+        theme: "dark-green",
+      },
+    },
+  ],
+  siteMetadata: {
+    description: "Personal page of Bilal Tariq",
+    locale: "en",
+    title: "Bilal Tariq",
+  },
+};
