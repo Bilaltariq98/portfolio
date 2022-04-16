f94d0ddff0b8c470a29869c0cc5ce6f163bbc9d4
Bilal Tariq <bilaltariq690@hotmail.com>
2022-04-16 20:53:04 +0100

    formspree configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,5 +11,6 @@ module.exports = {
     description: "Personal page of Bilal Tariq",
     locale: "en",
     title: "Bilal Tariq",
+    formspreeEndpoint: "https://formspree.io/f/mqknygbl"
   },
 };
